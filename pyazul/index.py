562dd22b9335d2341743969eb899752bb6b1d4aa
Cristopher Ortega <ortega.cpp@gmail.com>
2020-06-01 16:42:23 -0400

    [REF] Improving endpoints handling

--- pyazul/index.py
+++ pyazul/index.py
@@ -24,7 +24,7 @@ class AzulAPI():
         self.ALT_PRODUCTION_URL = 'https://contpagos.azul.com.do/Webservices/JSON/default.aspx'
         
 
-    def azul_request(self, transaction_type, data):
+    def azul_request(self, data, operation=''):
         try:
             
             # Required parameters for all transactions
@@ -41,26 +41,11 @@ class AzulAPI():
             print(
                 f'You are missing {missing_key} which is a required parameter.')
             return
-
-        if transaction_type == 'void':
-                self.TEST_URL += '?processvoid'
-                self.PRODUCTION_URL += '?processvoid'
-                self.ALT_PRODUCTION_URL += '?processvoid'
             
-        elif transaction_type == 'post':
-            self.TEST_URL += '?processpost'
-            self.PRODUCTION_URL += '?processpost'
-            self.ALT_PRODUCTION_URL += '?processpost'
-
-        elif transaction_type == 'datavault_create' or 'datavault_delete':
-            self.TEST_URL += '?ProcessDatavault'
-            self.PRODUCTION_URL += '?ProcessDatavault'
-            self.ALT_PRODUCTION_URL += '?ProcessDatavault'
-
         if self.ENVIRONMENT == 'prod':
-            azul_endpoint = self.PRODUCTION_URL
+            azul_endpoint = self.PRODUCTION_URL + f'?{operation}'
         else:
-            azul_endpoint = self.TEST_URL
+            azul_endpoint = self.TEST_URL + f'?{operation}'
 
         cert_path = self.config.certificate_path
 
@@ -75,7 +60,7 @@ class AzulAPI():
                                      headers=headers, cert=cert_path)
         except Exception as err:
             try:
-                azul_endpoint = self.ALT_PRODUCTION_URL
+                azul_endpoint = self.ALT_PRODUCTION_URL + f'{operation}'
                 response = requests.post(azul_endpoint, json=parameters,
                                          headers=headers, cert=cert_path)
                 print(str(err))
@@ -88,36 +73,36 @@ class AzulAPI():
 
     def sale_transaction(self, **kwargs):
         kwargs.update(validate.sale_transaction(kwargs))
-        return self.azul_request('sale', kwargs)
+        return self.azul_request(kwargs)
 
     def hold_transaction(self, **kwargs):
         kwargs.update(validate.hold_transaction(kwargs))
-        return self.azul_request('hold', kwargs)
+        return self.azul_request(kwargs)
 
     def refund_transaction(self, **kwargs):
         kwargs.update(validate.refund_transaction(kwargs))
-        return self.azul_request('refund', kwargs)
+        return self.azul_request(kwargs)
 
     def void_transaction(self, **kwargs):
         kwargs.update(validate.void_transaction(kwargs))
-        return self.azul_request('void', kwargs)
+        return self.azul_request(kwargs, operation='processvoid')
 
     def post_sale_transaction(self, **kwargs):
         kwargs.update(validate.post_sale_transaction(kwargs))
-        return self.azul_request('post', kwargs)
+        return self.azul_request(kwargs, operation='processpost')
 
     def verify_transaction(self, **kwargs):
         kwargs.update(validate.verify_transaction(kwargs))
-        return self.azul_request('verify', kwargs)
+        return self.azul_request(kwargs)
     
     def nulify_transaction(self, **kwargs):
         kwargs.update(validate.nullify_transaction(kwargs))
-        return self.azul_request('nullify', kwargs)
+        return self.azul_request(kwargs)
     
     def datavault_create(self, **kwargs):
         kwargs.update(validate.datavault_create(kwargs))
-        return self.azul_request('datavault_create', kwargs)
+        return self.azul_request(kwargs, operation='ProcessDatavault')
     
     def datavault_delete(self, **kwargs):
         kwargs.update(validate.datavault_delete(kwargs))
-        return self.azul_request('datavault_delete', kwargs)
+        return self.azul_request(kwargs, operation='ProcessDatavault')
