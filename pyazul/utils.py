dc4f78323490b72e2610c47d523f7200e4248670
Jorge Massih <jorgmassih@gmail.com>
2023-09-16 15:40:37 -0400

    [FIX]: fix small bug with `update_itbis` function

--- pyazul/utils.py
+++ pyazul/utils.py
@@ -5,9 +5,9 @@ def clean_amount(amount):
 # This function makes sure to not add 'Itbis' in the dict, unless there's a value.
 # Azul documentation doesn't specificy that it will fail if sent empty.
 def update_itbis(data):
-    if data.get('Itbis'):
+    if (itbis := data.get('Itbis')):
         temp = {
-            'Itbis': clean_amount(value),
+            'Itbis': clean_amount(itbis),
         }
         data.update(temp)
     return data
