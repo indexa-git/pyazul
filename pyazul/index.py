d71890c866a4fcd190cb26fe807be13ef6aaea38
crisog <ortega.cpp@gmail.com>
2024-07-19 11:50:26 -0400

    refactor: use pydantic for validation

--- pyazul/index.py
+++ pyazul/index.py
@@ -1,207 +1,101 @@
-# Python packages
-import json
 import logging
-
-# Third party packages
+from typing import Optional, Literal
+from pydantic import BaseModel, HttpUrl, Field
 import httpx
-from . import validate
+
+from .models import (
+    SaleTransactionModel,
+    HoldTransactionModel,
+    PostSaleTransactionModel,
+    RefundTransactionModel,
+    VoidTransactionModel,
+    VerifyTransactionModel,
+    DataVaultCreateModel,
+    DataVaultDeleteModel,
+)
 
 _logger = logging.getLogger(__name__)
 
+class AzulAPIConfig(BaseModel):
+    auth1: str
+    auth2: str
+    certificate_path: Optional[str] = Field(default=None) # This is mandatory, but made it optional for unit tests
+    custom_url: Optional[HttpUrl] = None
+    environment: Literal["dev", "prod"] = "dev"
 
 class AzulAPI:
-    def __init__(self, auth1, auth2, certificate_path, custom_url=None, environment='dev'):
-        '''
-        :param auth1
-        :param auth2
-        :param certificate_path (path to your .p12 certificate)
-        :param environment (string, defaults 'dev' can also be set to 'prod')
-        :param custom_url (string, defaults None, custom azul webservice url)
-        '''
-        self.certificate_path = (
-            certificate_path  # TODO validate this is an actual certificate
-        )
-        self.auth1 = auth1
-        self.auth2 = auth2
-        self.ENVIRONMENT = environment
+    def __init__(self, config: AzulAPIConfig):
+        self.config = config
+        self.url = config.custom_url or self._get_default_url()
 
-        if custom_url:
-            self.url = custom_url
-        else:
-            if environment == 'dev':
-                self.url = 'https://pruebas.azul.com.do/webservices/JSON/Default.aspx'
-            else:
-                self.url = 'https://pagos.azul.com.do/webservices/JSON/Default.aspx'
-                self.ALT_URL = 'https://contpagos.azul.com.do/Webservices/JSON/default.aspx'
+    def _get_default_url(self) -> str:
+        if self.config.environment == "dev":
+            return "https://pruebas.azul.com.do/webservices/JSON/Default.aspx"
+        return "https://pagos.azul.com.do/webservices/JSON/Default.aspx"
 
-    def azul_request(self, data, operation=''):
-        #  Required parameters for all transactions
-        parameters = {
-            'Channel': data.get('Channel', ''),
-            'Store': data.get('Store', ''),
-        }
-
-        # Updating parameters with the extra parameters
-        parameters.update(data)
-
-        azul_endpoint = self.url + f'?{operation}'
-        cert_path = self.certificate_path
-
+    async def azul_request(self, data: BaseModel, operation: str = ""):
+        azul_endpoint = f"{self.url}?{operation}"
         headers = {
-            'Content-Type': 'application/json',
-            'Auth1': self.auth1,
-            'Auth2': self.auth2,
+            "Content-Type": "application/json",
+            "Auth1": self.config.auth1,
+            "Auth2": self.config.auth2,
         }
-        r = {}
-        _logger.debug('azul_request: called with data:\n%s', data)
+        _logger.debug(f"azul_request: called with data:\n{data.model_dump_json()}")
+
+        client_kwargs = {}
+        if self.config.certificate_path:
+            client_kwargs["cert"] = self.config.certificate_path
 
         try:
-            r = httpx.post(
-                azul_endpoint,
-                json=parameters,
-                headers=headers,
-                cert=cert_path,
-                timeout=30,
-            )
-            if r.raise_for_status() and self.ENVIRONMENT == 'prod' and custom_url is None:
-                azul_endpoint = self.ALT_URL + f'?{operation}'
-                r = httpx.post(
-                    azul_endpoint,
-                    json=parameters,
-                    headers=headers,
-                    cert=cert_path,
-                    timeout=30,
-                )
-        except Exception as err:
-            _logger.error(
-                'azul_request: Got the following error\n%s', str(err))
-            raise Exception(str(err))
-
-        response = json.loads(r.text)
-        _logger.debug('azul_request: Values received\n%s', json.loads(r.text))
-
-        return response
-
-    def sale_transaction(self, data):
-        data.update(validate.sale_transaction(data))
-        return self.azul_request(data)
-
-    def hold_transaction(self, data):
-        data.update(validate.hold_transaction(data))
-        return self.azul_request(data)
-
-    def refund_transaction(self, data):
-        data.update(validate.refund_transaction(data))
-        return self.azul_request(data)
-
-    def void_transaction(self, data):
-        data.update(validate.void_transaction(data))
-        return self.azul_request(data, operation='ProcessVoid')
-
-    def post_sale_transaction(self, data):
-        data.update(validate.post_sale_transaction(data))
-        return self.azul_request(data, operation='ProcessPost')
-
-    def verify_transaction(self, data):
-        data.update(validate.verify_transaction(data))
-        return self.azul_request(data, operation='VerifyPayment')
-
-    def nulify_transaction(self, data):
-        data.update(validate.nullify_transaction(data))
-        return self.azul_request(data)
-
-    def datavault_create(self, data):
-        data.update(validate.datavault_create(data))
-        return self.azul_request(data, operation='ProcessDatavault')
-
-    def datavault_delete(self, data):
-        data.update(validate.datavault_delete(data))
-        return self.azul_request(data, operation='ProcessDatavault')
-
-
-class AzulAPIAsync(AzulAPI):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    async def azul_request(self, data, operation=''):
-        #  Required parameters for all transactions
-        parameters = {
-            'Channel': data.get('Channel', ''),
-            'Store': data.get('Store', ''),
-        }
-
-        # Updating parameters with the extra parameters
-        parameters.update(data)
-
-        azul_endpoint = self.url + f'?{operation}'
-        cert_path = self.certificate_path
-
-        headers = {
-            'Content-Type': 'application/json',
-            'Auth1': self.auth1,
-            'Auth2': self.auth2,
-        }
-        r = {}
-        _logger.debug('azul_request: called with data:\n%s', data)
-
-        try:
-            async with httpx.AsyncClient(cert=cert_path) as client: 
+            async with httpx.AsyncClient(**client_kwargs) as client:
                 r = await client.post(
                     azul_endpoint,
-                    json=parameters,
+                    json=data.model_dump(),
                     headers=headers,
                     timeout=30,
                 )
-                if r.raise_for_status() and self.ENVIRONMENT == 'prod':
-                    azul_endpoint = self.ALT_URL + f'?{operation}'
-                    r = httpx.post(
-                        azul_endpoint,
-                        json=parameters,
-                        headers=headers,
-                        timeout=30,
-                    )
+                await r.raise_for_status()
+        except httpx.HTTPStatusError as err:
+            if self.config.environment == "prod" and not self.config.custom_url:
+                alt_url = "https://contpagos.azul.com.do/Webservices/JSON/default.aspx"
+                azul_endpoint = f"{alt_url}?{operation}"
+                r = await client.post(
+                    azul_endpoint,
+                    json=data.model_dump(),
+                    headers=headers,
+                    timeout=30,
+                )
+                await r.raise_for_status()
+            else:
+                raise
         except Exception as err:
-            _logger.error(
-                'azul_request: Got the following error\n%s', str(err))
-            raise Exception(str(err))
+            _logger.error(f"azul_request: Got the following error\n{err}")
+            raise
 
-        response = json.loads(r.text)
-        _logger.debug('azul_request: Values received\n%s', json.loads(r.text))
-
+        response = r.json()
+        _logger.debug(f"azul_request: Values received\n{response}")
         return response
 
-    async def sale_transaction(self, data):
-        data.update(validate.sale_transaction(data))
+    async def sale_transaction(self, data: SaleTransactionModel):
         return await self.azul_request(data)
 
-    async def hold_transaction(self, data):
-        data.update(validate.hold_transaction(data))
+    async def hold_transaction(self, data: HoldTransactionModel):
         return await self.azul_request(data)
 
-    async def refund_transaction(self, data):
-        data.update(validate.refund_transaction(data))
+    async def refund_transaction(self, data: RefundTransactionModel):
         return await self.azul_request(data)
 
-    async def void_transaction(self, data):
-        data.update(validate.void_transaction(data))
-        return await self.azul_request(data, operation='ProcessVoid')
+    async def void_transaction(self, data: VoidTransactionModel):
+        return await self.azul_request(data, operation="ProcessVoid")
 
-    async def post_sale_transaction(self, data):
-        data.update(validate.post_sale_transaction(data))
-        return await self.azul_request(data, operation='ProcessPost')
+    async def post_sale_transaction(self, data: PostSaleTransactionModel):
+        return await self.azul_request(data, operation="ProcessPost")
 
-    async def verify_transaction(self, data):
-        data.update(validate.verify_transaction(data))
-        return await self.azul_request(data, operation='VerifyPayment')
+    async def verify_transaction(self, data: VerifyTransactionModel):
+        return await self.azul_request(data, operation="VerifyPayment")
 
-    async def nulify_transaction(self, data):
-        data.update(validate.nullify_transaction(data))
-        return await self.azul_request(data)
+    async def datavault_create(self, data: DataVaultCreateModel):
+        return await self.azul_request(data, operation="ProcessDatavault")
 
-    async def datavault_create(self, data):
-        data.update(validate.datavault_create(data))
-        return await self.azul_request(data, operation='ProcessDatavault')
-
-    async def datavault_delete(self, data):
-        data.update(validate.datavault_delete(data))
-        return await self.azul_request(data, operation='ProcessDatavault')
+    async def datavault_delete(self, data: DataVaultDeleteModel):
+        return await self.azul_request(data, operation="ProcessDatavault")
