3ff31c025a3031a29b0595003c033ec2052169f9
Joel German <joelgerman671@gmail.com>
2025-02-04 10:42:33 -0400

    Refactoring "env.copy" and adding hold test a test_void

--- tests/services/test_void.py
+++ tests/services/test_void.py
@@ -1,5 +1,5 @@
 import pytest
-from pyazul.models.schemas import VoidTransactionModel
+from pyazul.models.schemas import VoidTransactionModel, HoldTransactionModel
 from pyazul.services.transaction import TransactionService
 from pyazul.core.config import get_azul_settings
 
@@ -12,12 +12,30 @@ def transaction_service():
     settings = get_azul_settings()
     return TransactionService(settings)
 
+
+
 @pytest.mark.asyncio
 async def test_void_transaction(transaction_service):
+
+    """
+    Test the hold transaction method.
+    """
+    hold_transaction = HoldTransactionModel(
+        CardNumber='5413330089600119',
+        Expiration='202812',
+        CVC='979',
+        Amount='1000',
+        Itbis='100',
+        TrxType='Hold',
+        CustomOrderId='hold_test_to_post'
+    )
+    hold_result = await transaction_service.hold(hold_transaction)
+    assert hold_result['IsoCode'] == '00', "Hold transaction should be successful"
+    azul_order_id = hold_result['AzulOrderId']
     """
     Test the void transaction method.
     """
-    transaction = VoidTransactionModel(AzulOrderId='44474225', store=transaction_service.settings.AZUL_MERCHANT_ID)
+    transaction = VoidTransactionModel(AzulOrderId=azul_order_id, store=transaction_service.settings.AZUL_MERCHANT_ID)
     result = await transaction_service.void(transaction)
     assert result is not None
     assert result['IsoCode'] == '00', "Transaction should be voided successfully"
