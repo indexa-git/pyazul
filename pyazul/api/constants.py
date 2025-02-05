cb99a76fd785668bdafb72a205bf9e7c93065e22
Joel German <joelgerman671@gmail.com>
2025-02-05 17:42:59 -0400

    adding the routes to payment-page and install fastapi to test it

--- pyazul/api/constants.py
+++ pyazul/api/constants.py
@@ -14,6 +14,8 @@ class AzulEndpoints:
     DEV_URL = "https://pruebas.azul.com.do/webservices/JSON/Default.aspx"
     PROD_URL = "https://pagos.azul.com.do/webservices/JSON/Default.aspx"
     ALT_PROD_URL = "https://contpagos.azul.com.do/Webservices/JSON/default.aspx"
+    DEV_URL_PAYMEMT = "https://pruebas.azul.com.do/PaymentPage/"
+    PROD_URL_PAYMEMT = "https://pagos.azul.com.do/PaymentPage/"
 
     @classmethod
     def get_url(cls, environment: Environment) -> str:
