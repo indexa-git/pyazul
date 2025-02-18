8c9518cccef67993d4ba5f98f63264ee1e44bdf6
Joel German <joelgerman671@gmail.com>
2025-02-18 14:37:12 -0400

    test(SecureService) implement a test for 3D secure implementations

--- pyazul/examples/payment_page_server.py
+++ pyazul/examples/payment_page_server.py
@@ -96,4 +96,4 @@ async def buy_ticket(request: Request):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=3000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
